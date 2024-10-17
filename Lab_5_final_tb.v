0b56cfb6898c30227bffb33fc0af07468d28bbba
tla226 <144627052+tla226@users.noreply.github.com>
2024-10-17 18:07:04 -0400

    Add files via upload

--- Lab_5_final_tb.v
+++ Lab_5_final_tb.v
@@ -21,10 +21,7 @@
 
 
 
-/*
 
-to get waveform, add anode, bcd_seg, and g_count from uut and uut1, then show enable to show it only works on back half when enable is high
-*/
 `timescale 1ns/1ps
 module multi_seg_driver_tb;
 reg clk;
@@ -33,7 +30,6 @@ wire [3:0] sseg_a_o_tb; // output
 wire [6:0] sseg_c_o_tb;
 
 
-// Instantiate original module (named DUT {device under test})
 multi_seg_driver uut1(clk, BCD_in_tb, sseg_a_o_tb, sseg_c_o_tb);
 
 
@@ -45,7 +41,6 @@ always begin
     clk = 1'b1;
     #(T/2);
 end
-// if not go back to #80
 initial begin 
 BCD_in_tb = 16'b0000000000000000;
 #256 BCD_in_tb = 16'b0001001000110100;
